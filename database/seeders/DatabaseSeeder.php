12bc276d3cbac691f8fed80655ef52a8e027ec10
Michael Luna <michaelluna244@gmail.com>
2024-02-13 04:28:35 -0600

    added seeder for DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Pizza;
+use App\Models\Topping;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //Populating the DB
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $pizza1 = Pizza::factory()->create([
+            'name' => 'Classic Pepperoni',
+            'sauce' => true,
+            'cheese' => true,
+        ]);
+
+        $pizza1->toppings()->attach(Topping::factory()->create(['name' => 'Pepperoni']));
+
+        $pizza2 = Pizza::factory()->create([
+            'name' => 'Mushroom Madness',
+            'sauce' => true,
+            'cheese' => true,
+        ]);
+
+        $pizza2->toppings()->attach(Topping::factory()->create(['name' => 'Mushrooms']));
+
+        $pizza3 = Pizza::factory()->create([
+            'name' => 'Sausage Pizza',
+            'sauce' => true,
+            'cheese' => true,
+        ]);
+
+        $pizza3->toppings()->attach(Topping::factory()->create(['name' => 'Sausage']));
+
+        $pizza4 = Pizza::factory()->create([
+            'name' => 'Veggie Delight',
+            'sauce' => true,
+            'cheese' => true,
+        ]);
+
+        $pizza4->toppings()->attach(Topping::factory()->create(['name' => 'Bell Peppers']));
+
+        $pizza5 = Pizza::factory()->create([
+            'name' => 'Hawaiian Paradise',
+            'sauce' => true,
+            'cheese' => true,
+        ]);
+
+        $pizza5->toppings()->attach(Topping::factory()->create(['name' => 'Pineapple']));
     }
 }
